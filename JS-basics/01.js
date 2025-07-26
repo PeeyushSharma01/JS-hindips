774f1e23b5fd8fb63d3fd4852f24e11366c5e861
Peeyush Sharma <peeyush.sharmamtr@gmail.com>
2025-07-26 09:41:35 +0000

    01run

--- JS-basics/01.js
+++ JS-basics/01.js
@@ -1,1 +1,12 @@
-console.log("hello js")
+const accId=9737126
+let emailId="hitesh@gmail.com"
+let pwd="673278@7838"
+let mobileNo=7423462471
+cityname="Jaipur"
+                                //accId=4374832
+                                //console.log(accId);
+cityname="Banglore"
+console.table([accId, emailId, pwd, mobileNo, cityname]);
+
+
+
