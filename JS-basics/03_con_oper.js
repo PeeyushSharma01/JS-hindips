90a827375704b4e7c8b2bec0c05d5845394b7b7f
Peeyush Sharma <peeyush.sharmamtr@gmail.com>
2025-08-12 19:50:13 +0000

    conoper

---
+++ JS-basics/03_con_oper.js
@@ -0,0 +1,12 @@
+let firstnumber = null                    //"null"   //"" //"5500"    //44
+//console.log(typeof firstnumber)
+//console.log(typeof (firstnumber))  //method 
+//console.log(firstnumber)
+
+//let stringnumber = Number(firstnumber)
+//console.log(typeof stringnumber)
+//console.log(stringnumber)
+
+let booleannum = Boolean(firstnumber)   //1 / 7484->true   "hitesh" / "null"/"1121"->true || ""/ 0/null ->false 
+console.log(booleannum)
+
