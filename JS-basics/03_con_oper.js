3f0edf56c65646672ddf333b4bb5d05480734c3c
Peeyush Sharma <peeyush.sharmamtr@gmail.com>
2025-08-29 12:18:54 +0000

    code

--- JS-basics/03_con_oper.js
+++ JS-basics/03_con_oper.js
@@ -15,11 +15,11 @@ let firstnumber = null                    //"null"   //"" //"5500"    //44
 // console.log(mydate.toLocaleString());
 // console.log(typeof mydate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
 
 
 
