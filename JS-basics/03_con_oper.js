4dc616ebd7dd63620c84b6ab8ea88cfbc422690c
Peeyush Sharma <peeyush.sharmamtr@gmail.com>
2025-08-29 12:18:06 +0000

    prograqmmer hacker

--- JS-basics/03_con_oper.js
+++ JS-basics/03_con_oper.js
@@ -7,6 +7,21 @@ let firstnumber = null                    //"null"   //"" //"5500"    //44
 //console.log(typeof stringnumber)
 //console.log(stringnumber)
 
-let booleannum = Boolean(firstnumber)   //1 / 7484->true   "hitesh" / "null"/"1121"->true || ""/ 0/null ->false 
-console.log(booleannum)
+// let booleannum = Boolean(firstnumber)   //1 / 7484->true   "hitesh" / "null"/"1121"->true || ""/ 0/null ->false 
+// console.log(booleannum)
+// let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+
+
+
 
